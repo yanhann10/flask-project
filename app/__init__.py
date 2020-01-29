3f87eef92e19e68d25d1e0fd8a966cc31fd8bff4
hannah yan han <yanhann10@gmail.com>
2020-01-29 16:28:14 -0500

    set up summarizer

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,10 @@
 from flask import Flask
 
+
 app = Flask(__name__, instance_relative_config=True)
 
+
+    
 # Load the views
 from app import views
 # Load the config file
--- app/views.py
+++ app/views.py
@@ -1,34 +1,47 @@
-from flask import render_template
-from flask import jsonify
+from flask import Flask, render_template, session, redirect, url_for, session, jsonify
 from app import app
 import pandas as pd
-import wikipedia
+import os
+# FORM
+from flask_wtf import FlaskForm
+from wtforms import TextField, SubmitField
+# TEXT
+from gensim.summarization.summarizer import summarize
+from gensim.summarization import keywords
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "flask-266401-988bfe311e55.json"
+SECRET_KEY = os.urandom(32)
+app.config['SECRET_KEY'] = SECRET_KEY
 
-@app.route('/')
+# define input
+
+
+class ReadingForm(FlaskForm):
+    sep_len = TextField('Enter a list of readings')
+    output_len = TextField('Enter desired output in number of words')
+    submit = SubmitField('Analyze')
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template("index.html")
+
+    # Create instance of the form.
+    form = ReadingForm()
+    # Check validity of the form
+    if form.validate_on_submit():
+        session['sep_len'] = form.sep_len.data
+        session['output_len'] = form.output_len.data
+        return redirect(url_for("summarizeTxt"))
+    return render_template('index.html', form=form)
 
 
-@app.route('/pandas')
-def pandas_sugar():
-    df = pd.read_csv("https://raw.githubusercontent.com/noahgift/sugar/master/data/education_sugar_cdc_2003.csv")
-    return jsonify(df.to_dict())
+@app.route('/summarizeTxt')
+def summarizeTxt():
+    txt = session['sep_len']
+    wrd = session['output_len']
+    smry = summarize(txt, word_count=int(wrd))
+    # return smry
+    return render_template('smry.html', smry=smry)
 
-@app.route('/wikipedia/<company>')
-def wikipedia_route(company):
-
-    # Imports the Google Cloud client library
-    from google.cloud import language
-    from google.cloud.language import enums
-    from google.cloud.language import types
-    result = wikipedia.summary(company, sentences=10)
-
-    client = language.LanguageServiceClient()
-    document = types.Document(
-        content=result,
-        type=enums.Document.Type.PLAIN_TEXT)
-    entities = client.analyze_entities(document).entities
-    return str(entities)
 
 @app.route('/about')
 def about():
